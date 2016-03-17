4621a247a1c8053620c55a31b5d26524f60412da
Steve McLaughlin <stevejrmc@gmail.com>
2016-03-17 14:13:00 -0700

    I fixed my dice

--- rotate-box.js
+++ rotate-box.js
@@ -1,5 +1,5 @@
 var init = function() {
-  var box = document.querySelector('#theArt').children[0],
+  var box = document.querySelector('#dieOne').children[0],
       showPanelButtons = document.querySelectorAll('#show-buttons input'),
       panelClassName = 'show-front',
 
@@ -16,3 +16,22 @@ var init = function() {
 };
   
 window.addEventListener( 'DOMContentLoaded', init, false);
+
+var init2 = function() {
+  var box = document.querySelector('#dieTwo').children[0],
+      showPanelButtons = document.querySelectorAll('#show-buttons2 input'),
+      panelClassName = 'show-front',
+
+      onButtonClick = function( event ){
+        box.removeClassName( panelClassName );
+        panelClassName = event.target.className;
+        box.addClassName( panelClassName );
+      };
+
+  for (var i=0, len = showPanelButtons.length; i < len; i++) {
+    showPanelButtons[i].addEventListener( 'click', onButtonClick, false);
+  }
+    
+};
+
+window.addEventListener( 'DOMContentLoaded', init2, false);
